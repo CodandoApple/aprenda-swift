1fd177e23d3f7fa032cd0fd0133c2de263dfb1bf
Matheus Reis <matheus.reis@bancointer.com.br>
2022-04-13 18:54:01 -0300

    Adicionando fila de links na action de validação

--- Tests/LinkValidatorTests/main.swift
+++ Tests/LinkValidatorTests/main.swift
@@ -4,6 +4,8 @@ import XCTest
 
 class LinksTest : XCTestCase {
     
+    var queue: [String] = []
+    var triesCount: [String:Int] = [:]
     var regex: NSRegularExpression?
     
     override func setUp() {
@@ -16,39 +18,61 @@ class LinksTest : XCTestCase {
     
     func testLinks() throws {
         
-        let text = try LinkValidator.shared.readFileText("README.md")
+        do {
+            let text = try LinkValidator.shared.readFileText("README")
+            
+            XCTAssertNotNil(text, "Foi impossível ler o arquivo README.md")
+            XCTAssertGreaterThan(text.count, 0, "O arquivo está vazio")
+            
+            self.queue = extractLinksFromText(text)
+            
+            XCTAssertGreaterThan(queue.count, 0, "URLS não encontrados no arquivo")
+            
+            while !queue.isEmpty {
+                guard let link = queue.last, let url = URL(string: link) else { continue }
+                
+                let expectation = XCTestExpectation(description: "Carregar a página \(link)")
+                
+                let task = URLSession.shared.dataTask(with: url, completionHandler: { (data,response,error) in
+                    XCTAssertNil(error, "A página \(link) não foi encontrada")
+                    
+                    if let httpResponse = response as? HTTPURLResponse {
+                        XCTAssertTrue((200...299).contains(httpResponse.statusCode),"A página \(link) não está disponível")
+                        let _ = self.queue.popLast()
+                    } else {
+                        if let count = self.triesCount[link] {
+                            if count == 3 {
+                                let _ = self.queue.popLast()
+                                XCTFail("A página \(link) está fora do ar")
+                            } else {
+                                self.triesCount[link] = count + 1
+                            }
+                        } else {
+                            self.triesCount[link] = 1
+                        }
+                    }
+                    expectation.fulfill()
+                })
+                task.resume()
+                wait(for: [expectation], timeout: 10.0)
+            }
+        } catch (let error) {
+            XCTFail(error.localizedDescription)
+        }
         
-        XCTAssertNotNil(text, "Foi impossível ler o arquivo README.md")
-        XCTAssertGreaterThan(text.count, 0, "O arquivo está vazio")
-        
+    }
+}
+
+extension LinksTest {
+    private func extractLinksFromText(_ text: String) -> [String] {
+        guard let regex = regex else { return [] }
         let textRange = NSRange(text.startIndex..., in: text)
-        let links: [String]? = regex?.matches(in: text, options: [], range: textRange)
+        
+        return regex.matches(in: text, options: [], range: textRange)
             .map {
                 let matchRange = Range($0.range(withName: "link"), in: text)!
                 return String(text[matchRange])
             }.filter { $0.starts(with: "http") }
-        
-        XCTAssertGreaterThan(links?.count ?? 0, 0, "URLS não encontrados no arquivo")
-        XCTAssertNotNil(links, "A lista de links não foi obtida")
-        
-        for link in links! {
-            let expectation = XCTestExpectation(description: "Carregar a página \(link)")
-
-            let url = URL(string: link)!
-            let task = URLSession.shared.dataTask(with: url, completionHandler: { (data,response,error) in
-                XCTAssertNil(error, "A página \(link) não foi encontrada")
-                if let httpResponse = response as? HTTPURLResponse {
-                    XCTAssertEqual(httpResponse.statusCode, 200,
-                                   String(format: "A página %@ não está disponível", httpResponse.url?.absoluteString ?? ""))
-                } else {
-                    XCTFail(String(format: "A página %@ falhou", link))
-                }
-                expectation.fulfill()
-            })
-            task.resume()
-            wait(for: [expectation], timeout: 10.0)
-        }
-        
     }
 }
 
